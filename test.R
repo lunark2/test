72b80595eaeaa7e3ba79f9bf8a429b9c565a8fe1
Simin Lee <lunark321@gmail.com>
2022-11-15 14:59:21 +0900

    init

---
+++ test.R
@@ -0,0 +1,9 @@
+# 로컬 프로젝트를 github로 연동하기
+
+# 1. 먼저 예시로 새로운 R 프로젝트를 생성하고 R 파일을 저장해보자
+
+# 2. 나의 github에서 새로운 R 프로젝트와 연동시킬 repository를 만든다. (중요: readme, ignore, license를 체크하지 않고 만든다)
+## 생성된 repository에서 보이는 git remote add origin부분을 복사한다.
+## 예시는 git@github.com:lunark2/test.git
+
+# 3. Tools-Project options-Git/SVN에 가서 version control을 git으로 변경
